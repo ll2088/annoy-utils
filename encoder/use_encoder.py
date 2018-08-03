421fa3abaf701213cdac1a4795dfc4740310b2be
Vineet Kumar <vineet.mundhra@gmail.com>
2018-08-03 11:15:34 +0530

    change logging

--- encoder/use_encoder.py
+++ encoder/use_encoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 logging = tf.logging
-logging.set_verbosity(logging.ERROR)
+logging.set_verbosity(logging.INFO)
 
 
 TFHUB_URL_USE = 'https://tfhub.dev/google/universal-sentence-encoder/1'
@@ -21,7 +21,7 @@ class USEEncoder(object):
         self.embedding_fun = embed(self.sentences)
         self.sess = tf.Session()
         self.sess.run([tf.global_variables_initializer(), tf.tables_initializer()])
-        print(f'USE Encoder Ready! large: {large}')
+        logging.info(f'USE Encoder Ready! large:{large}')
 
     def encode(self, sentences):
         if not sentences:
